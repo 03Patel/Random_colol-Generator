65b8f95ed2cf980f1c23ece68f4ba5cfa63fd6a8
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2024-05-30 12:04:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+let h1=document.querySelector("h1");
+let h2=document.querySelector("span");
+let btn=document.querySelector("button");
+let body=document.querySelector("body");
+// let color=["red","green","blue"];
+
+btn.addEventListener("click",()=>{
+
+    let ranbtn=randomColor();
+     document.body.style.backgroundColor=ranbtn;
+     h2.innerHTML=ranbtn;
+
+
+})
+
+// function randomColor(){
+
+//         let letters = '0123456789ABCDEF'.split('');
+//         let key = '#';
+//         for (var i = 0; i < 6; i++ ) {
+//           key += letters[Math.floor(Math.random() * 16)];
+//         }
+//         return key;
+      
+// }
+function randomColor(){
+  let i=Math.floor(Math.random()*256);
+  let j=Math.floor(Math.random()*256);
+  let k=Math.floor(Math.random()*256);
+  let color=`rgb(${i},${j},${k})`;
+  return color;
+}
